6c8373e0b19c70cb29d996f8d3eefb7cfd86e435
sevskii <sevskii111@gmail.com>
2020-02-17 11:54:11 +0300

    finished lab 3 code

---
+++ 3/threads/threader.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *task1()
+{
+	for (int i = 1;i <= 10;i++)
+	{
+		printf("Hello Threads (%i)\n", i);
+		sleep(1);
+	}
+}
+
+void *task2()
+{
+	for (int i = 1;i <= 12;i++)
+	{
+		printf("This is iteration %i\n", i);
+		sleep(2);
+	}
+}
+
+int main()
+{
+	pthread_t thread1, thread2;
+	int res1 = pthread_create(&thread1, NULL, task1, NULL);
+	int res2 = pthread_create(&thread2, NULL, task2, NULL);
+	
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL); 
+	return 0;
+}
