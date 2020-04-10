0c671f03165765507bbd42cff603df681eef484e
sevskii <sevskii111@gmail.com>
2020-04-10 20:28:08 +0300

    lab10 code RC1

---
+++ 10/signals.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+
+int sigint_c = 0, sigusr1_c = 0, sigusr2_c = 0;
+
+void handl_sig(int nsig)
+{
+  switch (nsig)
+  {
+  case SIGINT:
+    sigint_c++;
+    break;
+
+  case SIGUSR1:
+    sigusr1_c++;
+    break;
+
+  case SIGUSR2:
+    sigusr2_c++;
+    break;
+  default:
+    break;
+  }
+}
+
+int main()
+{
+  signal(SIGTERM, SIG_IGN);
+  signal(SIGINT, handl_sig);
+  signal(SIGUSR1, handl_sig);
+  signal(SIGUSR2, handl_sig);
+  while (1)
+  {
+    size_t startTime = time(NULL);
+    while (time(NULL) - startTime < 1 && sigint_c < 5) usleep(100);
+    printf("Time: %d\n", time(NULL));
+    printf("SIGINT: %d\n", sigint_c);
+    printf("SIGUSR1: %d\n", sigusr1_c);
+    printf("SIGUSR2: %d\n", sigusr2_c);
+    if (sigint_c >= 5)
+      break;
+    sleep(1);
+  }
+}
