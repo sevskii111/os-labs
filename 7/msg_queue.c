c5dfecd468688014ebd972a7313b8da3a22ebcce
sevskii <sevskii111@gmail.com>
2020-04-10 21:09:39 +0300

    lab7 code fix

--- 7/msg_queue.c
+++ 7/msg_queue.c
@@ -9,8 +9,7 @@
 
 struct thread_args
 {
-  size_t msgId1;
-  size_t msgId2;
+  size_t msgId;
 };
 
 struct my_msg
@@ -31,10 +30,10 @@ void print_nums(char *nums, int c)
 void *mainTask(void *thread_args)
 {
   struct thread_args *args = (struct thread_args *)thread_args;
-  size_t msgId1 = args->msgId1;
-  size_t msgId2 = args->msgId2;
+  size_t msgId = args->msgId;
   struct my_msg send;
   struct my_msg recive;
+  send.mtype = 1;
 
   char nums[4];
   srand((unsigned)(time(0)));
@@ -43,26 +42,24 @@ void *mainTask(void *thread_args)
     nums[i] = rand() % 10;
   }
   print_nums(nums, 4);
-  send.mtype = 1;
   for (int i = 0; i < 4; i++)
   {
     send.msg[i] = nums[i];
   }
-  msgsnd(msgId1, &send, sizeof(send), 0);
+  msgsnd(msgId, &send, sizeof(send), 0);
 
-  msgrcv(msgId2, &recive, sizeof(recive), 0, 0);
+  msgrcv(msgId, &recive, sizeof(recive), 2, 0);
   int p_count = recive.msg[0];
   printf("Permutations:\n");
   for (int i = 0; i < p_count; i++)
   {
-    msgrcv(msgId2, &recive, sizeof(recive), 0, 0);
+    msgrcv(msgId, &recive, sizeof(recive), 2, 0);
     print_nums(recive.msg, 4);
   }
 
   printf("Permutations count:%d\n", p_count);
 
-  msgctl(msgId1, IPC_RMID, NULL);
-  msgctl(msgId2, IPC_RMID, NULL);
+  msgctl(msgId, IPC_RMID, NULL);
 
   return NULL;
 }
@@ -70,19 +67,12 @@ void *mainTask(void *thread_args)
 void *childTask(void *thread_args)
 {
   struct thread_args *args = (struct thread_args *)thread_args;
-  size_t msgId1 = args->msgId1;
-  size_t msgId2 = args->msgId2;
+  size_t msgId = args->msgId;
   struct my_msg send;
   struct my_msg recive;
+  send.mtype = 2;
 
-  char nums[4];
-
-  msgrcv(msgId1, &recive, sizeof(recive), 0, 0);
-
-  for (int i = 0; i < 4; i++)
-  {
-    nums[i] = recive.msg[i];
-  }
+  msgrcv(msgId, &recive, sizeof(recive), 1, 0);
 
   char p[24][4];
   int p_count = 0;
@@ -98,10 +88,10 @@ void *childTask(void *thread_args)
           continue;
         int l = 6 - i - j - k;
         char curr_p[4];
-        curr_p[0] = nums[i];
-        curr_p[1] = nums[j];
-        curr_p[2] = nums[k];
-        curr_p[3] = nums[l];
+        curr_p[0] = recive.msg[i];
+        curr_p[1] = recive.msg[j];
+        curr_p[2] = recive.msg[k];
+        curr_p[3] = recive.msg[l];
         char unique = 1;
         for (int m = 0; m < p_count; m++)
         {
@@ -119,15 +109,13 @@ void *childTask(void *thread_args)
       }
     }
   }
-  send.mtype = 2;
   send.msg[0] = p_count;
-  msgsnd(msgId2, &send, sizeof(send), 0);
-  send.mtype = 1;
+  msgsnd(msgId, &send, sizeof(send), 0);
   for (int i = 0; i < p_count; i++)
   {
     memcpy(send.msg, p[i], 4);
 
-    msgsnd(msgId2, &send, sizeof(send), 0);
+    msgsnd(msgId, &send, sizeof(send), 0);
   }
 
   return NULL;
@@ -139,8 +127,7 @@ int main()
   int msgId2 = msgget(IPC_PRIVATE, 0600 | IPC_CREAT);
 
   struct thread_args *args;
-  args->msgId1 = msgId1;
-  args->msgId2 = msgId2;
+  args->msgId = msgId1;
 
   pthread_t mainThread, childThread;
 
